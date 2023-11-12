170afc21bc98209b59075f90ad0073f5d2a62e81
Evan Zhao <zhw2513953778@outlook.com>
2023-11-12 00:22:50 -0800

    Add tower generate

--- Quest3 Demo App/Assets/_Script/GameStateManager.cs
+++ Quest3 Demo App/Assets/_Script/GameStateManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc8d6771c72c6d13b8b7507574b68089bc932472488a101422c458916bff124c
-size 2971
+oid sha256:1413a8b5a621c79a18fbdf7932c7143c6cfeff77f52a1ac4b003d836802fe63c
+size 4052
--- Quest3 Demo App/Assets/_Script/MonumentGenerator.cs
+++ Quest3 Demo App/Assets/_Script/MonumentGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adf5afe3181e5b99922e8d417ec3441e924f7bd05a017f6ba26d915d9e64d206
-size 511
+oid sha256:b229b3850c11906fbe478e52c48276e1ca12a226f717d854c8b74a769c4c9764
+size 693
