9bed16d295cff85cf803fa1f470c1d5e3888286c
CrazYN00dleS <terrys19990@gmail.com>
2023-11-12 01:58:31 -0800

    fixed enemy spawner, add tag to monument

--- Quest3 Demo App/Assets/_Script/EnemySpawner.cs
+++ Quest3 Demo App/Assets/_Script/EnemySpawner.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01ad75b0dd81da6bbe685a3cb2ff021706795d73f86e34ff7a6976aa12e4338d
-size 1407
+oid sha256:b0aba665da6165245b4e1c024bca853a2a4599eaa03f3d7898d13b47aa42d54b
+size 1494
